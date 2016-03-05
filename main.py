845bcee9961a7876cf171dc7a88c15d8cd30bd82
Nate Symington <nrs084@gmail.com>
2016-03-06 00:10:24 +1300

    added utf8, to handle superstrings and unicode characters Added freefall section, equation needs testing

--- main.py
+++ main.py
@@ -1,46 +1,121 @@
+# coding: utf-8
+import math
+
 GLOBAL_CONSTANT_GRAVITY = -9.81
 
 
 def main():
-    vi = 9.81  # initial velocity in m/s
-    vf = 0  # final velocity in m/s
-    a = GLOBAL_CONSTANT_GRAVITY
 
-    rocket_up_time(vi, vf, a)
+    vi = 9.81   # initial velocity in m/s
+    vf = 0      # final velocity in m/s
+    a = GLOBAL_CONSTANT_GRAVITY # displayed acceleration (a) or gravity (g)
 
+    rocket_up_calc(vi, vf, a)
 
-def rocket_up_time(vi, vf, a):
-    """ Calculate distance and time it takes a rocket to get up before it stops midair
-        take initial velocity, final velocity and acceleration as arguments """
-
-    print("velocity intial vi = {0} m/s".format(vi))
-    print("velocity final vf = {0} m/s".format(vf))
-    print("acceleration (gravity) = {0} m/s^-2".format(a))
-
-    print(u"\u2588" * 30)
-
-    print("The formula to get the DISTANCE (d) travelled before it will stop midair")
-    print(u"\u2591" + " (vf^2 - vi^2) / 2a " + u"\u2591")
-    print("change in velocity ({0}^2 - {1}^2) m/s = {2} m/s".format(vf, vi, vf ** 2 - vi ** 2))
-    print("acceleration is constant so multiply by 2 (2 x {0}) ms^-2 = {1} m/s^-2".format(a, 2 * a))
-
-    print("DISTANCE travelled = {0} m".format((vf ** 2 - vi ** 2) / (2 * a)))
-
-    print(u"\u2588" * 30)
-
-    print("The formula to get the TIME (t) travelled before it will stop midair")
-    print(u"\u2591" + " (vf - vi) / a " + u"\u2591")
-    print('change in velocity ({0} - {1}) m/s = {2} m/s'.format(vf, vi, vf - vi))
-    print("acceleration is just gravity acting down ({0}) ms^-2 = {0} m/s^-2".format(a))
-    print("TIME travelled = {0} s".format((vf - vi) / (a)))
-
-    print(u"\u2588" * 30)  # http://www.fileformat.info/info/unicode/block/block_elements/images.htm
+    d0 = 29.5 # initial altitude/displacement (distance in m)
+    t0 = 1.55 # time in seconds, can use s or t for unit
+    #d0 = 35.5 # initial altitude/displacement (distance in m)
+    #t0 = 1.25 # time in seconds, use t or hr/m/s for unit
+    height_fallen_calc(d0, t0, a)
 
 
+def rocket_up_calc(vi, vf, a):
+    """Calculate distance and time it takes a rocket to get up before it stops midair
+        take initial velocity, final velocity and acceleration as arguments"""
+
+    print('velocity initial       vi = {0} m/s'.format(vi))
+    print('velocity final         vf = {0} m/s'.format(vf))
+    print('acceleration (gravity) a  = {0} m/s⁻²'.format(a))
+    print('█' * 30)
+
+    print('The formula to get the DISTANCE (d) travelled before it will stop midair')
+    print('░ d = (vf² - vi²) / 2a ░')
+    print('change in velocity:                        ({0}² - {1}²) m/s = {2} m/s'.format(vf, vi, vf ** 2 - vi ** 2))
+    print('acceleration is constant so multiply by 2: (2 x {0}) ms⁻² = {1} m/s⁻²'.format(a, 2 * a))
+    print('DISTANCE travelled (d) =                   {0} m'.format((vf ** 2 - vi ** 2) / (2 * a)))
+
+    print('█' * 30)
+
+    print('The formula to get the TIME (t) travelled before it will stop midair')
+    print('░ t = (vf - vi) / a ░')
+    print('change in velocity:     ({0} - {1}) m/s = {2} m/s'.format(vf, vi, vf - vi))
+    print('acceleration (gravity): ({0}) ms⁻² = {0} m/s⁻²'.format(a))
+    print('TIME travelled (t) =    {0} s'.format((vf - vi) / a))
+
+    print('█' * 30)  # http://www.fileformat.info/info/unicode/block/block_elements/images.htm
 
 
+def height_fallen_calc(d0, t0, a):
+    """Find the height something has fallen from, given the time and final distance before it hit the ground"""
+
+    df = 0 # height final distance in m (eg when it hits the ground) - (s) is the standard unit for displacement
+    vi = 0 # this is the initial velocity at which object started at, since it is free fall = 0 m/s
+
+    print('Calculate the initial DISTANCE (di) given a time (t0) and final altitude (d0)')
+    print('an object as last recorded at for when an object hits the ground from free-fall')
+    print("In layman's terms:")
+    print('A certain freely falling object requires {0} s to travel the last {1} m before it hits the ground.'.format(t0, d0))
+    print('From what height above the ground did it fall?')
+    print('█' * 30)
+    print('displacement/altitude/height initial di = {0} m'.format('unknown'))
+    print('displacement/altitude/height last    d0 = {0} m'.format(d0))
+    print('displacement/altitude/height final   df = {0} m'.format(df))
+
+    #print('time initial                         ti = {0} s'.format('unknown'))
+    print('time last                            t0 = {0} s'.format(t0))
+    #print('time final                           tf = {0} s'.format('unknown')
+
+    print('velocity initial (because dropped)   vi = {0} m/s'.format(vi))        # u = the initial velocity (speed)
+    print('velocity last                        v0 = {0} m/s'.format('unknown'))
+    print('velocity final                       vf = {0} m/s'.format('unknown')) # v = the final velocity
+
+    print('acceleration (gravity)               a  = {0} m/s⁻²'.format(a))
+    print('█' * 30)
 
 
+    print('The standard physics formula to find distance of (h) given the time change is:')
+    print('░ h = v0t + (½)gt² ░')
+    # print('The standard physics formula to find velocity of (v) given the time change is:')
+    # print('░ v = v0 + at ░')
 
+    print('Since last VELOCITY (v0) is unknown, find it by adjusting equation:')
+    print('░ d0 = v0(t0) + (½)a(t0)² ░')
+    print('░ v0 = (d0 -  (½)a(t0)²) / (t0) ░')
+
+    print('  v0 = ({0} - (½)*{1}*{2}²) / {2}'.format(d0, a, t0))
+    print('  v0 = ({0} - {1}) / {2}'.format(d0, 0.5 * a * t0 ** 2, t0))
+    print('  v0 = ({0}) / {1}'.format(d0 - 0.5 * a * t0 ** 2, t0))
+    print('░ v0 = {0} m/s ░'.format((d0 - 0.5 * a * t0 ** 2) / t0))
+
+    print('The kinematic equation to find VELOCITY (vf) given distance is:')
+    print('░ vf² = v0² + 2aΔx ░')
+    print('Calculate final VELOCITY (vf) when the object hits the ground')
+    print('  vf² = v0² + 2a(df-d0)')
+    print('  vf² = {0:.2f}² + 2*{1}*({2}-{3}) '.format((d0 - 0.5 * a * t0 ** 2) / t0, a, df, d0))
+    print('  vf² = {0:.2f} + {1}*({2}) '.format((d0 - 0.5 * a * t0 ** 2 / t0) ** 2, 2 * a , df - d0))
+    print('  vf² = {0:.2f} + {1} '.format((d0 - 0.5 * a * t0 ** 2 / t0) ** 2, (2 * a) * (df - d0)))
+    print('  vf² = {0} '.format((d0 - 0.5 * a * t0 ** 2 / t0) ** 2 + (2 * a) * (df - d0)))
+    print('  vf  = √{0} '.format((d0 - 0.5 * a * t0 ** 2 / t0) ** 2 + (2 * a) * (df - d0)))
+    print('░ vf  = {0} m/s ░'.format(math.sqrt((d0 - 0.5 * a * t0 ** 2 / t0) ** 2 + (2 * a) * (df - d0))))
+
+    print('The kinematic equation to find VELOCITY (vf) given distance (x) is:')
+    print('░ vf² = v0² + 2aΔx ░')
+    print('Since initial DISTANCE (di) is unknown, find it by adjusting equation:')
+    print('░ vf² = vi² + 2a(di) ░')
+    print('░ di  = (vi² - vf²) / 2a ░')
+
+    print('  di  = ({0}² - {1:.2f}²) / 2*{2}'.format(vi, (d0 - 0.5 * a * t0 ** 2) / t0, a))
+    print('  di  = ({0} - {1}) / {2}'.format(vi ** 2, ((d0 - 0.5 * a * t0 ** 2) / t0) ** 2, 2 * a))
+    print('  di  = ({0}) / {1}'.format((vi ** 2) - ((d0 - 0.5 * a * t0 ** 2) / t0) ** 2, 2 * a))
+    print('  di  = {0} m'.format((vi ** 2) - ((d0 - 0.5 * a * t0 ** 2) / t0) ** 2 / 2 * a))
 
 main()
+
+"""
+u = the initial velocity (speed)
+v = the final velocity
+Example: Car accelerates from 50mph to 70mph in 10seconds. What is the acceleration?
+Answer a = (v-u)/t
+= (70-50)/10
+= 0.5 mph / second^2
+"""
