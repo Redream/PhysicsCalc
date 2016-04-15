7b5d6643c36e64c6712eb4cf50c4a69c753d6e29
nsy16 <nrs084@gmail.com>
2016-04-15 12:01:08 +1200

    webassign 2.4

--- main.py
+++ main.py
@@ -97,20 +97,10 @@ def webassign_get_questions(week):
         1: ["Question 2",
             "Question 3",
             "Question 4"],
-        2: ["Question 1A / D",
-            "Question 1B / C",
-            "Question 2A",
-            "Question 2B",
-            "Question 3A",
-            "Question 3B",
-            "Question 4A",
-            "Question 4B",
-            "Question 4C",
-            "Question 4D",
-            "Question 4E",
-            "Question 5A",
-            "Question 5B",
-            "Question 5C"],
+        2: ["Question 1",
+            "Question 2",
+            "Question 3",
+            "Question 4"],
         3: ["Question 1A",
             "Question 1B",
             "Question 2A",
@@ -146,13 +136,35 @@ def webassign_do_question(week, question):
             else: original_height_fallen_from(xc=float(x), tc=float(t))
     elif week == 2:
         if question == 1:
-            distance = float(input("Please enter the distance (m): "))
-            force = float(input("Please enter the applied force (N): "))
-            angle = float(input("Please enter the angle (degrees): "))
-            answer = force * distance * math.cos(to_radians(angle))
-            print("Answer to a) and d): " + str(round(answer, 2)) + " J")
-        else:
-            print("Question not supported.")
+            m = input("Mass (m): ")
+            F = input("Force (F): ")
+            vf = input("Final velocity (km/h): ")
+            if not (m and F and vf): train_acceleration_time()
+            else: train_acceleration_time(mc=float(m), Fc=float(F), vfc=float(vf))
+        if question == 2:
+            a = input("Acceleration (m/s²): ")
+            m = input("Mass (kg): ")
+            if not (a and m): tension_two_masses_elevator()
+            else: tension_two_masses_elevator(mc=float(m), ac=float(a))
+        if question == 3:
+            m = input("Mass (kg): ")
+            v0 = input("Initial velocity (m/s): ")
+            vf = input("Final velocity (m/s): ")
+            t = input("Time (s): ")
+            if not (m and v0 and vf and t): force_distance_slowing_car()
+            else: force_distance_slowing_car(mc=float(m), v0c=float(v0), vfc=float(vf), tc=float(t))
+        if question == 4:
+            m1 = input("Mass 1 (kg): ")
+            m2 = input("Mass 2 (kg): ")
+            if not (m1 and m2): m2_gtr_m1_atwood_machine()
+            else: m2_gtr_m1_atwood_machine(m1c=float(m1), m2c=float(m2))
+        #else:
+            #distance = float(input("Please enter the distance (m): "))
+            #force = float(input("Please enter the applied force (N): "))
+            #angle = float(input("Please enter the angle (degrees): "))
+            #answer = force * distance * math.cos(to_radians(angle))
+            #print("Answer to a) and d): " + str(round(answer, 2)) + " J")
+            #print("Question not supported.")
     elif week == 3:
         if question == 10:
             m1 = float(input("Please input mass m1 (kg): "))
@@ -214,39 +226,26 @@ def show_invalid_option():
     print("Invalid option.")
     input("Press enter to exit. ")
 
-def print_maths_multiply(a, b, c):
+
+def print_maths_evaluate(operator, a, b, c):
     """
-    Prints a, which is b multiplied by c
+    Prints a, which is b operated by c
     Returns a
     """
     if isinstance(b, (int, float, complex)):
-        print('{} = {} * {:{}} {}'.format(a[2], b, c[1], c[4], c[3]))
-        a[1] = b * c[1]
+        print('{} = {} {} {:{}} {}'.format(a[2], b, operator, c[1], c[4], c[3]))
+        a[1] = eval('{} {} {}'.format(b, operator, c[1]))
+        print('{} = {:{}} {}'.format(a[2], a[1], a[4], a[3]))
     elif isinstance(c, (int, float, complex)):
-        print('{} = {:{}} {} * {}'.format(a[2], b[1], b[4], b[3], c))
-        a[1] = b[1] * c
+        print('{} = {:{}} {} {} {}'.format(a[2], b[1], b[4], b[3], operator, c))
+        a[1] = eval('{} {} {}'.format(b[1], operator, c))
+        print('{} = {:{}}'.format(a[2], a[1], a[4]))
     else:
-        print('{} = {:{}} {} * {:{}} {}'.format(a[2], b[1], b[4], b[3], c[1], c[4], c[3]))
-        a[1] = b[1] * c[1]
-    print('{} = {:{}} {}'.format(a[2], a[1], a[4], a[3]))
+        print('{} = {:{}} {} {} {:{}} {}'.format(a[2], b[1], b[4], b[3], operator, c[1], c[4], c[3]))
+        a[1] = eval('{} {} {}'.format(b[1], operator, c[1]))
+        print('{} = {:{}} {}'.format(a[2], a[1], a[4], a[3]))
     return a
 
-def print_maths_divide(a, b, c):
-    """
-    Prints a, which is b divided by c
-    Returns a
-    """
-    if isinstance(b, (int, float, complex)):
-        print('{} = {} / {:{}} {}'.format(a[2], b, c[1], c[4], c[3]))
-        a[1] = b / c[1]
-    elif isinstance(c, (int, float, complex)):
-        print('{} = {:{}} {} / {}'.format(a[2], b[1], b[4], b[3], c))
-        a[1] = b[1] / c
-    else:
-        print('{} = {:{}} {} / {:{}} {}'.format(a[2], b[1], b[4], b[3], c[1], c[4], c[3]))
-        a[1] = b[1] / c[1]
-    print('{} = {:{}} {}'.format(a[2], a[1], a[4], a[3]))
-    return a
 
 def print_convert_kmh_ms(a):
     """
@@ -256,10 +255,25 @@ def print_convert_kmh_ms(a):
     a[1] = a[1] / 3.6
     print('{0} = {1:{2}} {4} / 3.6 = {1:{2}} {3}'.format(a[2], a[1], a[4], a[3], 'km/h'))
 
+def print_convert_ms_kmh(a):
+    """
+    Multiplies a by 3.6
+    Returns a
+    """
+    a[1] = a[1] * 3.6
+    print('{0} = {1:{2}} {3} * 3.6 = {1:{2}} {4}'.format(a[2], a[1], a[4], a[3], 'km/h'))
+
+
 def print_mechanics_motion1(solve, v, v0, a, t):
     """ velocity (without x): v = v₀ + at """
     print('velocity (without x): v = v₀ + at')
 
+    if solve[2] == a[2]:
+        print('rearrange to solve acceleration: a = Δv / t')
+        print('{} = ({:{}} {} - {:{}} {}) / {:{}} {}'.format(a[2], v[1], v[4], v[3], v0[1], v0[4], v0[3], t[1], t[4], t[3]))
+        a[1] = (v[1] - v0[1]) / t[1]
+        print('{} = {:{}} {}'.format(a[2], a[1], a[4], a[3]))
+        return a
     if solve[2] == t[2]:
         print('rearrange to solve time: t = Δv / a')
         print('{} = ({:{}} {} - {:{}} {}) / {:{}} {}'.format(t[2], v[1], v[4], v[3], v0[1], v0[4], v0[3], a[1], a[4], a[3]))
@@ -267,12 +281,26 @@ def print_mechanics_motion1(solve, v, v0, a, t):
         print('{} = {:{}} {}'.format(t[2], t[1], t[4], t[3]))
         return t
 
-def print_mechanics_motion2(solve, x, v0, v, a, t):
+def print_mechanics_motion2(solve, x, v0, v, t):
     """ change in x (without acceleration): Δx = ½(v₀ + v)t """
+    print('change in x (without acceleration): Δx = ½(v₀ + v)t')
+
+    if solve[2] == x[2]:
+        print('solve distance: Δx = ∑v*t / 2')
+        print('{} = (({:{}} {} + {:{}} {}) * {:{}} {}) / 2'.format(x[2], v0[1], v0[4], v0[3], v[1], v[4], v[3], t[1], t[4], t[3]))
+        x[1] = ((v0[1] + v[1]) * t[1]) / 2
+        print('{} = {:{}} {}'.format(x[2], x[1], x[4], x[3]))
+        return x
 
 def print_mechanics_motion3(solve, x, v0, t, a):
     """ change in x (without final velocity): Δx = v₀t + ½at """
     print('change in x (without final velocity): Δx = v₀t + ½at')
+
+    if solve[2] == x[2]:
+        print('{0} = ({1:{2}} {3} * {4:{5}} {6}) + (0.5 * {7:{8}} {9} * {4:{5}} {6})'.format(x[2], v0[1], v0[4], v0[3], t[1], t[4], t[3], a[1], a[4], a[3]))
+        x[1] = (v0[1] * t[1]) + (0.5 * a[1] * t[1])
+        print('{} = {:{}} {}'.format(x[2], x[1], x[4], x[3]))
+        return x
 
     if solve[2] == v0[2]:
         print('rearrange to solve initial velocity: v₀ = Δx - ½at² / t')
@@ -304,6 +332,36 @@ def print_mechanics_motion4(solve, v, v0, a, x):
         print('{} = {:{}} {}'.format(x[2], x[1], x[4], x[3]))
         return x
 
+def print_atwood_machine(solve, m1, m2, g, a, T):
+    """solve acceleration or tension in an atwood machine"""
+    print('In an atwood machine: T1 = T2')
+    print('where m2 > m1')
+    print('T - m1g = m1a')
+    print('m2g - T = m2a')
+    if solve[2] == a[2]:
+        print('rearrange to solve for a')
+        print('m2g - T + T - m1g = m1a + m2a')
+        print('m2g - m1g = m1a + m2a')
+        print('(m2 - m1)g = (m1 + m2)a')
+        print('a = (m2 - m1)g /(m1 + m2)')
+        print('{0} = (({4:{5}} {6} - {1:{2}} {3}) * {7:{8}} {9}) / ({1:{2}} {3} + {4:{5}} {6})'.format(a[2], m1[1], m1[4], m1[3], m2[1], m2[4], m2[3], g[1], g[4], g[3]))
+        a[1] = ((m2[1] - m1[1]) * g[1]) / (m1[1] + m2[1])
+        print('{} = {:{}} {}'.format(a[2], a[1], a[4], a[3]))
+        return a
+    if solve[2] == T[2]:
+        print('rearrange to solve for T')
+        print('a = (T - m1g) / m1')
+        print('a = (m2g - T) / m2')
+        print('(T - m1g) / m1 = (m2g - T) / m2')
+        print('m2(T - m1g) = m1(m2g - T)')
+        print('m2T - m1m2g = m1m2g - m1T')
+        print('m2T + m1T = m1m2g + m1m2g')
+        print('T(m2 + m1) = 2(m1m2g)')
+        print('T = (2(m1m2g))/(m2 + m1)')
+        print('{0} = (2 * {1:{2}} {3} * {4:{5}} {6} * {7:{8}} {9}) / ({4:{5}} {6} + {1:{2}} {3})'.format(T[2], m1[1], m1[4], m1[3], m2[1], m2[4], m2[3], g[1], g[4], g[3]))
+        T[1] = (2 * m1[1] * m2[1] * g[1]) / (m1[1] + m2[1])
+        print('{} = {:{}} {}'.format(T[2], T[1], T[4], T[3]))
+        return T
 
 def train_stopping_time(v0c=82.0, vfc=15.1, xc=350):
     """
@@ -362,7 +420,7 @@ def arrow_shot_up(v0c=119, vfc=0, ac=-9.81):
     # solve time: t = Δv / a
     print_mechanics_motion1(t, vf, v0, g, t)
     print('multiply by 2 as the arrow must return to earth')
-    print_maths_multiply(t, t, 2)
+    print_maths_evaluate('*', t, t, 2)
 
 def original_height_fallen_from(xc=25, tc=1.8, ac=9.81):
     """
@@ -396,11 +454,151 @@ def original_height_fallen_from(xc=25, tc=1.8, ac=9.81):
     # solve last velocity: v₀ = Δx - ½at² / t
     print_mechanics_motion3(v0, x0, v0, t0, g)
 
-    # velocity (without time): v² = v₀² + 2aΔx
+    # solve final velocity: v² = v₀² + 2aΔx
     print_mechanics_motion4(vf, vf, v0, g, x0)
 
     # rearrange to solve start height: Δx = (v² - v₀²) / 2a
     print_mechanics_motion4(xi, vf, vi, g, xi)
+
+def train_acceleration_time(mc=17000000, Fc=740000, vfc=76, v0c=0):
+    """
+    brief:    find time it takes for object to reach speed
+    from:     WebAssign Homework 2.1
+    category: force
+    types:    constant acceleration
+    """
+
+    m = ['mass', mc, 'm', 'kg', '.1e', 'scalar']
+    F = ['force', Fc, 'F', 'N', '.1e', 'scalar']
+    t = ['time', 0, 't', 's', '.1f', 'scalar']
+    v0 = ['initial velocity', v0c, 'v₀', 'm s⁻¹', '.2f', 'vector']
+    vf = ['final velocity', vfc, 'vf', 'm s⁻¹', '.2f', 'vector']
+    a = ['acceleration', 0, 'a', 'm s⁻²', '.2f', 'scalar']
+
+    print('A freight train has a mass of {:{}} {}.'.format(m[1], m[4], m[2]))
+    print('If the locomotive can exert a constant pull of {:{}} {}'.format(F[1], F[4], F[2]))
+    print('(a) how long does it take to increase the speed of the train from rest to {:{}} {}?'.format(vf[1], vf[4], 'km/h'))
+
+    print('░ question a ░')
+    print('convert km/h to m/s: x / 3.6')
+    print_convert_kmh_ms(vf)
+    print_convert_kmh_ms(v0)
+
+    print('acceleration: a = F/m')
+    print_maths_evaluate('/', a, F, m)
+
+    # solve change in t
+    print_mechanics_motion1(t, vf, v0, a, t)
+    print('convert to minutes: x / 60')
+    print_maths_evaluate('/', t, t, 60)
+
+def tension_two_masses_elevator(ac=2.30, mc=13.5, gc=9.81):
+    """
+    brief:    find tension in 2 attached masses in a moving elevator
+    from:     WebAssign Homework 2.2
+    category: tension force
+    types:    constant acceleration, opposing acceleration
+    """
+
+    m = ['mass', mc, 'm', 'kg', '.1f', 'scalar']
+    a = ['acceleration', ac, 'a', 'm s⁻²', '.2f', 'scalar']
+    g = ['gravity', gc, 'a', 'm s⁻²', '.2f', 'scalar']
+    ar = ['acceleration acting on the rope', ac, 'ar', 'm s⁻²', '.2f', 'scalar']
+    T1 = ['force', 0, 'F', 'N', '.2f', 'scalar']
+    T2 = ['force', 0, 'F', 'N', '.2f', 'scalar']
+    Em = ['combined mass acting on the top rope', mc, '∑m', 'kg', '.1f', 'scalar']
+
+    print('Two blocks are fastened to the ceiling of an elevator.')
+    print('The elevator accelerates upward at {:{}} {}. The blocks both have a mass of {:{}} {}.'.format(a[1], a[4], a[3], m[1], m[4], m[3]))
+    print('Find the tension in each rope.')
+    print('(a) top rope N')
+    print('(b) bottom rope N')
+
+    print('░ question a ░')
+    # tension will increase, because moving in opposite direction of gravity. combine acceleration and gravity
+    print_maths_evaluate('+', ar, a, g)
+    # tension in the top rope is the sum of all masses
+    print_maths_evaluate('+', Em, m, m)
+    # F = ma
+    print_maths_evaluate('*', T1, Em, ar)
+
+    print('░ question b ░')
+    # F = ma
+    print_maths_evaluate('*', T2, m, ar)
+
+def force_distance_slowing_car(mc=2000, v0c=19.0, vfc=10.0, tc=5.60):
+    """
+    brief:    find force and distance of a car decelerating
+    from:     WebAssign Homework 2.3
+    category: force
+    types:    constant negative acceleration
+    """
+
+    m = ['mass', mc, 'm', 'kg', '.0f', 'scalar']
+    v0 = ['initial velocity', v0c, 'v₀', 'm s⁻¹', '.1f', 'vector']
+    vf = ['final velocity', vfc, 'vf', 'm s⁻¹', '.1f', 'vector']
+    t = ['time', tc, 't', 's', '.2f', 'scalar']
+    F = ['force', 0, 'F', 'N', '.0f', 'scalar']
+    x = ['change in distance', 0, 'Δx', 'm', '.1f', 'scalar']
+    a = ['acceleration', 0, 'a', 'm s⁻²', '.2f', 'scalar']
+
+    print('-' * 30)
+    print('A {:{}} {} car is slowed down uniformly from {:{}} {} to {:{}} {} in {:{}} {}.'.format(m[1], m[4], m[3], v0[1], v0[4], v0[3], vf[1], vf[4], vf[3], t[1], t[4], t[3]))
+    print('(a) What average total force acted on the car during that time? N')
+    print('(b) How far did the car travel during that time? m')
+
+    print('░ question a ░')
+    # solve acceleration: a = Δv / t'
+    print_mechanics_motion1(a, vf, v0, a, t)
+    # F = ma
+    print_maths_evaluate('*', F, m, a)
+
+    print('░ question b ░')
+    # solve distance: Δx = ∑v*t / 2'
+    print_mechanics_motion2(x, x, v0, vf, t)
+
+
+def m2_gtr_m1_atwood_machine(m1c=1, m2c=6):
+    """
+    brief:    find forces on an unbalance atwood machine
+    from:     WebAssign Homework 2.4
+    category: atwood machine
+    types:    tension, force
+    """
+
+    m1 = ['mass1', m1c, 'm1', 'kg', '.2f', 'scalar']
+    m2 = ['mass2', m2c, 'm2', 'kg', '.2f', 'scalar']
+
+    T = ['tension', 0, 'T', 'N', '.1f', 'scalar']
+    a = ['acceleration', 0, 'a', 'm s⁻²', '.2f', 'scalar']
+    g = ['gravity', 9.81, 'a', 'm s⁻²', '.2f', 'scalar']
+
+    t = ['time', 0, 't', 's', '.2f', 'scalar']
+    x = ['change in distance', 0, 'Δx', 'm', '.2f', 'scalar']
+    v0 = ['initial velocity', 0, 'v₀', 'm s⁻¹', '.2f', 'vector']
+
+
+    print('-' * 30)
+    print('Two objects with masses of {:{}} {} and {:{}} {} are connected by a light string that passes over a frictionless pulley'.format(m1[1], m1[4], m1[3], m2[1], m2[4], m2[3]))
+    print('(a) Determine the tension in the string. N')
+    print('(b) Determine the acceleration of each object. m/s²')
+    print('(c) Determine the distance each object will move in the first second of motion if both objects start from rest. m')
+
+    print('░ question a ░')
+    print_atwood_machine(T, m1, m2, g, a, T)
+
+    print('░ question b ░')
+    print_atwood_machine(a, m1, m2, g, a, T)
+
+    print('░ question c ░')
+    # time is 1 second
+    t[1] = 1
+    # initial velocity = 0
+    v0[1] = 0
+    # solve distance: Δx = v₀t + ½at
+    print_mechanics_motion3(x, x, v0, t, a)
+
+
 
 
 def race_car_circular_track():
@@ -635,66 +833,16 @@ def wave_period_speed(Ac=0.09, hlc=0.20, fc=17.0):
 
     print('░ question c ░')
     print('wave period: T = 1/f')
-    print_maths_divide(T, 1, f)
+    print_maths_evaluate('/', T, 1, f)
 
     print('░ question d ░')
     print('speed of a wave: v = fλ')
-    print_maths_multiply(v, f, A)
+    print_maths_evaluate('*', v, f, A)
 
 main()
 
 
-def template():
-    """
-    brief:
-    from:     WebAssign Homework
-    category:
-    types:
-    """
 
-    # related to the
-    M = [3.0, 'kg']  # M: mass of spool
-    R = [0.6, 'm']  # r: radius length
-    w = [0, 'rad s⁻¹']  # ω: angular velocity     - (ω = v/r)
-    I = [0, 'kg m²']  # I: moment of inertia    - (I = MR²) * default model
-
-    # related to the
-    mB = [3.0, 'kg']  # mB: mass of bucket
-    d = [4.8, 'm']  # d: distance travelled
-    g = [9.81, 'm s⁻²']  # g: gravity
-
-    # related to dimensions of the circle
-    r = [444, 'm']  # r: radius length - m
-
-    # related to angular momentum in the center of the circle
-    O = [0, 'rad']  # θ: angular displacement - rad     (θ = ωt)
-    w = [0, 'rad s⁻¹']  # ω: angular velocity     - rad s⁻¹ (ω = v/r)
-    a = [0, 'rad s⁻²']  # α: angular acceleration - rad s⁻² (α = ω/r)
-
-    # related to the kinematics of the car travelling on the circumference of the circle
-    d = [0, 'm']  # d: distance travelled       - m     (d = rθ)
-    v = [0, 'm s⁻¹']  # v: velocity                 - m s⁻¹ (v = rω)
-    aT = [0.34, 'm s⁻²']  # aT: tangential acceleration - m s⁻² (aT = v²/r) OR (aT = rα)
-    t = [0, 's']  # t: time                     - s
-
-    # related to the spring
-    k = [10.0, 'N m⁻¹']  # k: spring constant
-    E = [0, 'J']  # E: spring potential energy (E = ½kx²)
-    E0 = [0, 'J']  # E0: spring potential energy at equilibrium position
-    KE = [0, 'J']  # KE: kinetic energy (KE = ½mv²)
-
-    # related to the mass
-    m = [0.04, 'kg']  # m: mass
-    x = [0.20, 'm']  # x: displacement
-    v = [0, 'm s⁻¹']  # v: velocity
-
-    print('-' * 30)
-    print("")
-    print("")
-    print("")
-    print("")
-
-    print('░ question a ░')
 
 
 """
